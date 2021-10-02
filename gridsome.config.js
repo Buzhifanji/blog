8aa4652a51db072400cb1fd875a2a75a3c49f582
buzhifanji <buzhifanji@163.com>
2021-10-02 12:20:07 +0800

    feat: 替换blog技术栈为gridsome

---
+++ gridsome.config.js
@@ -0,0 +1,11 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: '不知凡几',
+  siteDescription: '笔记', // 网站的基本介绍，有利于搜索引擎
+  plugins: []
+}
