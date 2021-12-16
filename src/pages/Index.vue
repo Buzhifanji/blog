d840261e43d3ec151c7d01f4d8baa7e28b9133ae
huangbin <buzhifanji@163.com>
2021-12-16 15:47:55 +0800

    修改 blog 首页内容

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -5,6 +5,11 @@
         <div class="flex flex-col items-center mb-2 text-ui-primary">
           <Logo :width="80" />
           <h2 class="text-3xl text-6xl font-black tracking-tighter border-none">学习计算机笔记</h2>
+          <p class="text-xl font-medium text-center">
+            好记性不如烂笔头
+            <br />
+            <!-- <strong class="text-ui-primary">Got a minute?</strong> -->
+          </p>
         </div>
         <!-- <h1 class="text-4xl text-center lg:text-5xl">
           Great Documentation starts here.
@@ -39,32 +44,76 @@
       ></div>
 
       <div class="flex flex-wrap justify-center -mx-4">
-        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
-          <ZapIcon size="3x" class="mb-6 text-ui-primary" />
-          <h3 class="font-bold tracking-wide uppercase text-ui-primary">代码片段</h3>
-          <g-link to="/code-snippet/">
-            <p class="text-lg text-left">个人代码记录库</p>
-          </g-link>
-        </div>
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <CodeIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">代码片段</h3>
+          <p class="text-lg text-left">个人代码记录库</p>
+        </g-link>
 
-        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
-          <SearchIcon size="3x" class="mb-6 text-ui-primary" />
-          <h3 class="font-bold tracking-wide uppercase text-ui-primary">Instant Search</h3>
-          <p class="text-lg text-left">
-            All headlines are indexed. Instant search powered by Fuse.js. Find what you are looking for in the
-            <span
-              class="border-b border-dashed border-ui-primary text-ui-primary"
-            >blink of an eye</span>.
-          </p>
-        </div>
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <ChromeIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">浏览器</h3>
+          <p class="text-lg text-left">浏览器相关知识</p>
+        </g-link>
 
-        <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <WifiIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">网络</h3>
+          <p class="text-lg text-left">网络</p>
+        </g-link>
+
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <MapIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">数据结构</h3>
+          <p class="text-lg text-left">用JavaScript实现数据结构</p>
+        </g-link>
+
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <HashIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">算法</h3>
+          <p class="text-lg text-left">记录刷LeetCode的笔记</p>
+        </g-link>
+
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <CoffeeIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">JS</h3>
+          <p class="text-lg text-left">深入理解JavaScript</p>
+        </g-link>
+
+        <g-link
+          to="/code-snippet/"
+          class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3"
+        >
+          <TriangleIcon size="3x" class="mb-6 text-ui-primary" />
+          <h3 class="font-bold tracking-wide uppercase text-ui-primary pt-8">vue</h3>
+          <p class="text-lg text-left">主要是阅读vue源码笔记</p>
+        </g-link>
+
+        <!-- <div class="flex flex-col items-center w-full px-4 mb-8 text-center md:w-1/3">
           <MoonIcon size="3x" class="mb-6 text-ui-primary" />
           <h3 class="font-bold tracking-wide uppercase text-ui-primary">Lights out</h3>
           <p
             class="text-lg text-left"
           >Working late at night again? Enable dark mode with the click of a button.</p>
-        </div>
+        </div>-->
       </div>
     </div>
   </Layout>
@@ -72,14 +121,21 @@
 
 <script>
 import Logo from '@/components/Logo';
-import { ArrowRightCircleIcon, CodeIcon, MoonIcon, SearchIcon, ZapIcon } from 'vue-feather-icons';
+import { ArrowRightCircleIcon, ChromeIcon, CodeIcon, CoffeeIcon, HashIcon, MapIcon, MoonIcon, SearchIcon, TriangleIcon, WifiIcon, ZapIcon } from 'vue-feather-icons';
 
 export default {
   components: {
+    CodeIcon,
+    ChromeIcon,
+    WifiIcon,
+    MapIcon,
+    HashIcon,
+    CoffeeIcon,
+    TriangleIcon,
+
     Logo,
     ArrowRightCircleIcon,
     ZapIcon,
-    CodeIcon,
     MoonIcon,
     SearchIcon
   },
