fab85c223ed692fb7096e6205d1b28cecc4c6b2a
buzhifanji <buzhifanji@163.com>
2021-12-30 20:51:04 +0800

    删除多余代码

--- docs/.vuepress/.temp/register-components/clientAppEnhance.044c1612.js
+++
@@ -1,5 +0,0 @@
-import { defineAsyncComponent } from 'vue'
-
-export default ({ app }) => {
-  app.component("home", defineAsyncComponent(() => import("/Users/huangbin/Documents/my_project/blog/docs/.vuepress/components/home.vue")))
-}
