30499f45b7127c8e84e935251ee05314ab3d50d3
buzhifanji <buzhifanji@163.com>
2022-03-04 02:07:35 +0800

    update4

--- deploy.sh
+++ deploy.sh
@@ -26,7 +26,7 @@ fi
 git init
 git add -A
 git commit -m "${msg}"
-git push -f $githubUrl gh-pages # 推送到github gh-pages分支
+git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
 
 # deploy to coding pages
 # echo 'www.xugaoyi.com\nxugaoyi.com' > CNAME  # 自定义域名
